206c53beb4df097873f465117076c86157b54252
yrslove <yrslovee@gmail.com>
2018-10-19 20:40:00 +0300

    add cdn vis.ja

--- index2.php
+++ index2.php
@@ -1,5 +1,6 @@
 <?php
-
+header("Access-Control-Allow-Origin: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
